3093da54cbf0db2771f2013dff2198258b0a44ec
Mohamed Jeylani <95252871+mjeylanii@users.noreply.github.com>
2024-05-02 17:31:24 -0400

    Refactor unit tests in 1_unit-tests.js file

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -8,18 +8,6 @@ suite("Unit Tests", () => {
   suiteSetup(() => {
     solver = new Solver();
   });
-  // Logic handles a valid puzzle string of 81 characters
-  // Logic handles a puzzle string with invalid characters (not 1-9 or .)
-  // Logic handles a puzzle string that is not 81 characters in length
-  // Logic handles a valid row placement
-  // Logic handles an invalid row placement
-  // Logic handles a valid column placement
-  // Logic handles an invalid column placement
-  // Logic handles a valid region (3x3 grid) placement
-  // Logic handles an invalid region (3x3 grid) placement
-  // Valid puzzle strings pass the solver
-  // Invalid puzzle strings fail the solver
-  // Solver returns the expected solution for an incomplete puzzle
 
   test("Logic handles a valid puzzle string of 81 characters", function () {
     let puzzle = puzzlesAndSolutions[0][0];
